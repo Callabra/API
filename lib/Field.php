67ac100ef10ecceedbd20d5c6080eec45bc08b8e
Tyler Mothershead <tylermothershead@gmail.com>
2017-01-22 15:28:14 -0700

    new Field Class for metadata.

---
+++ lib/Field.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Callabra;
+
+class Field extends Callabra {
+
+	function meta($module,$field) {
+
+		self::setModule("fields");
+		self::setAction("meta");
+
+
+		self::addParameter("fields","module",$module);
+		self::addParameter("fields","field",$field);
+
+		$result = self::send();
+
+		return $result;	
+
+	}
+
+	function states($country) {
+
+		self::setModule("fields");
+		self::setAction("states");
+
+
+		self::addParameter("fields","country",$country);
+		#self::addParameter("fields","field",$field);
+
+		$result = self::send();
+
+		return $result;	
+
+	}
+
+
+
+
+
+
+}
+
+?><?php
+
+namespace Callabra;
+
+class Field {
+
+	function meta($field) {
+
+		self::setModule("fields");
+		self::setAction("meta");
+
+
+		self::addParameter("fields","field",$field);
+
+		$result = self::send();
+
+		return $result;	
+
+	}
+
+
+
+
+
+
+
+
+}
+
+?>
