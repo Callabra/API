0774d52f92d5b38039fc4088e900dadb843db274
Tyler Mothershead <tylermothershead@gmail.com>
2017-09-03 11:49:48 -0600

    updated subscription functions.

--- lib/Subscription.php
+++ lib/Subscription.php
@@ -18,7 +18,27 @@ class Subscription extends Callabra  {
 		return $result;			
 	}
 
-	public static function create($account, $plan, $setup_fee = null, $quantity = 1, $coupon = null, $trial_end = null)
+
+	public static function check($account, $plan, $coupon = null, $quantity = 1, $setup_fee = null, $trial_end = null) {
+
+		self::setModule("subscriptions");
+		self::setAction("check");
+
+		self::addParameter("subscriptions", "account" , $account);
+		self::addParameter("subscriptions", "plan" , $plan);
+		self::addParameter("subscriptions", "coupon" , $coupon);
+		self::addParameter("subscriptions", "quantity" , $quantity);
+		self::addParameter("subscriptions", "trial_end" , $trial_end);
+
+
+		$result = self::send();
+
+		return $result;
+
+	}
+
+
+	public static function create($account, $plan, $coupon = null, $quantity = 1, $setup_fee = null, $trial_end = null)
 	{
 
 		self::setModule("subscriptions");
