cd88f67746455ce365b18d87005e593f9997c9ba
Tyler Mothershead <tylermothershead@gmail.com>
2020-09-21 12:02:27 -0600

    added Email class

---
+++ lib/Email.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Callabra;
+
+class Email extends Callabra  {
+
+
+	public static function create($to, $subject, $body )
+	{
+
+		self::setModule("emails");
+		self::setAction("create");
+
+		self::addParameter("emails","to",$to);
+		self::addParameter("emails","subject",$subject);
+		self::addParameter("emails","body",$body);
+
+		$result = self::send();
+
+		return $result;			
+	}	
+
+
+
+}
+?>
